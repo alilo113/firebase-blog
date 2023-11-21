b9c600b3b78e2c4a83d62300b9e3732d961e5d68
unknown <aliopdandan@gmail.com>
2023-11-21 16:18:52 +0100

    commit

--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -21,11 +21,10 @@ const db = getDatabase(app)
 const reference = ref(db, "posts/" + crypto.randomUUID());  
   
 const storage = getStorage()
-const storageRef = ref(storage)
-const imagesRef = ref(storage, "/images")
 
 export function NewPost() {
   const [title, setTitle] = useState("");
+  const [image, setImage] = useState(null);
   const [content, setContent] = useState("");
   const nav = useNavigate()
 
@@ -51,49 +50,61 @@ export function NewPost() {
     } else {
       console.log("Please provide a title and content for the post.");
     }
-  };    
+  };
   
   return (
     <form
       className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md"
       onSubmit={handleSubmit}
     >
-        <h1 className="text-3xl font-semibold mb-4">New Post</h1>
-        <div className="mb-4">
-          <label htmlFor="title" className="block text-gray-700 font-semibold mb-2">
-            Title
-          </label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            id="title"
-            type="text"
-            name="title"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-500"
-            placeholder="Enter the title..."
-          />
-        </div>
-        <div className="mb-4">
-        </div>
-        <div className="mb-4">
-          <label htmlFor="content" className="block text-gray-700 font-semibold mb-2">
-            Content
-          </label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            id="content"
-            name="content"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 h-32 resize-none focus:outline-none focus:border-blue-500"
-            placeholder="Write your content here..."
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
-        >
-          Publish
-        </button>
-      </form>
-    );
-  }  
+      <h1 className="text-3xl font-semibold mb-4">New Post</h1>
+      <div className="mb-4">
+        <label htmlFor="title" className="block text-gray-700 font-semibold mb-2">
+          Title
+        </label>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          id="title"
+          type="text"
+          name="title"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="Enter the title..."
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="image" className="block text-gray-700 font-semibold mb-2">
+          Image
+        </label>
+        <input
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          type="file"
+          id="image"
+          name="image"
+          accept="image/*"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="content" className="block text-gray-700 font-semibold mb-2">
+          Content
+        </label>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          id="content"
+          name="content"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 h-32 resize-none focus:outline-none focus:border-blue-500"
+          placeholder="Write your content here..."
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+      >
+        Publish
+      </button>
+    </form>
+  );  
+}  
