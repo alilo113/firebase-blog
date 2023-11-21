29e08faa8901bb0f31a669ebf70dccdae6913300
unknown <aliopdandan@gmail.com>
2023-11-21 14:48:46 +0100

    commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,43 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { Link } from "react-router-dom";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+const firebaseConfig = {
+  databaseURL: "https://blog-2-c7269-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "AIzaSyAzrno79zHWlmX5dQ71Fs24ai8jVw88EEU",
+  authDomain: "blog-2-c7269.firebaseapp.com",
+  projectId: "blog-2-c7269",
+  storageBucket: "blog-2-c7269.appspot.com",
+  messagingSenderId: "515644145492",
+  appId: "1:515644145492:web:f7420331365b3030dcd2c1",
+  measurementId: "G-TX12R6EJ3K"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app)
+const reference = ref(db, "posts/" + "blog");  
 
 export function Home() {
+  const [posts, setPosts] = useState([]);
 
-  async function fetchData(){
-    try {
-      const url = "https://blog-2-c7269-default-rtdb.europe-west1.firebasedatabase.app/.json";
-      const res = await fetch(url)
-      const data = await res.json()
-      console.log(data.posts)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  console.log(fetchData())
+  useEffect(() => {
+    const postsRef = ref(db, "posts");
+    onValue(postsRef, (snapshot) => {
+      const postsData = snapshot.val();
+      if (postsData) {
+        const postsArray = Object.keys(postsData).map((key) => ({
+          id: key,
+          ...postsData[key],
+        }));
+        setPosts(postsArray);
+      }
+    });
+  }, []);
 
   return (
     <>
+      {/* Your existing UI */}
       <div className="text-white bg-zinc-900 flex items-center justify-between p-5">
         <h1 className="text-xl">personal blog</h1>
         <Link to={'/newpost'} className="hover:underline">
@@ -24,15 +45,17 @@ export function Home() {
         </Link>
       </div>
       <div className="flex flex-wrap justify-center">
-          <div className="border border-solid border-black w-fit p-5 m-5">
-            <img alt="Blog" className="max-w-xs" />
+        {posts.map((post) => (
+          <div key={post.id} className="border border-solid border-black w-fit p-5 m-5">
+            <img src={post.imageUrl} alt="Blog" className="max-w-xs" />
             <div className="flex items-center justify-between my-4">
-              <h1 className="text-2xl font-bold">Blog 1</h1>
-              <Link className="bg-sky-800 p-3 text-white rounded-md hover:bg-sky-500">
+              <h1 className="text-2xl font-bold">{post.title}</h1>
+              <Link to={`/post/${post.id}`} className="bg-sky-800 p-3 text-white rounded-md hover:bg-sky-500">
                 LEARN MORE
               </Link>
             </div>
           </div>
+        ))}
       </div>
     </>
   );
--- src/components/newpost.jsx
+++ src/components/newpost.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, push, set } from "firebase/database";
 import { useNavigate } from "react-router-dom";
-  
-  const firebaseConfig = {
+import { getStorage, ref as storageRef, uploadBytes } from "firebase/storage";
+
+const firebaseConfig = {
     databaseURL: "https://blog-2-c7269-default-rtdb.europe-west1.firebasedatabase.app",
     apiKey: "AIzaSyAzrno79zHWlmX5dQ71Fs24ai8jVw88EEU",
     authDomain: "blog-2-c7269.firebaseapp.com",
@@ -15,35 +16,43 @@ import { useNavigate } from "react-router-dom";
   };
   
   const app = initializeApp(firebaseConfig);
+  const storage = getStorage(app);
   const db = getDatabase(app)
   const reference = ref(db, "posts/" + crypto.randomUUID());  
   
   export function NewPost() {
     const [title, setTitle] = useState("");
-    const [imageFile, setImageFile] = useState(null);
     const [content, setContent] = useState("");
     const nav = useNavigate()
 
-    async function getData(e){
+    const handleSubmit = async (e) => {
       e.preventDefault();
-      if(reference){
-        await set(reference, {
-          title: title,
-          content: content,
-        }, console.log("data has been sendet"));
+      if (title && content) {
+        const postsRef = ref(db, "posts");
+        const newPostRef = push(postsRef);
+    
+        if (newPostRef) {
+          await set(newPostRef, {
+            title: title,
+            content: content,
+            // Add other post properties like image URL if needed
+          });
+          
+          setTitle("");
+          setContent("");
+          nav("/");
+        } else {
+          console.log("Failed to create a new post reference.");
+        }
+      } else {
+        console.log("Please provide a title and content for the post.");
       }
-      else{
-        console.log("no data has been sent")
-      }
-      setTitle("");
-      setContent("");
-      nav("/")
-    }
-  
+    };    
+
     return (
       <form
         className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md"
-        onSubmit={getData}
+        onSubmit={handleSubmit}
       >
         <h1 className="text-3xl font-semibold mb-4">New Post</h1>
         <div className="mb-4">
@@ -61,15 +70,6 @@ import { useNavigate } from "react-router-dom";
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="image" className="block text-gray-700 font-semibold mb-2">
-            Image
-          </label>
-          <input
-            id="image"
-            type="file"
-            name="image"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-500"
-          />
         </div>
         <div className="mb-4">
           <label htmlFor="content" className="block text-gray-700 font-semibold mb-2">
